bb7eb8e40160c47e72b8362c926e4a4fbba9fadd
hmsanket <hmsanket.926@gmail.com>
2025-11-02 14:09:15 +0530

    Delete src directory

--- src/pages/Auth/ResetPassword.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const ResetPassword = () => (
-  <div style={{maxWidth:520, margin:'40px auto', padding:24, background:'#fff', borderRadius:8}}>
-    <h3>Reset Password (mock)</h3>
-    <p>Reset form placeholder.</p>
-  </div>
-);
-export default ResetPassword;
